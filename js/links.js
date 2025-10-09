508051e4a62a4602a20f6e30dcc941eac1d1c9c5
fofariak <32439453+fofariak@users.noreply.github.com>
2025-10-09 15:10:38 +0530

    Update links.js

--- js/links.js
+++ js/links.js
@@ -4,8 +4,8 @@
 const productLinks = {
   // Time & Age Related Products
   'star-maps': 'https://amzn.to/3IRmdjq',
-  'time-capsule': 'https://www.amazon.com/dp/YOUR_PRODUCT_ID?tag=YOUR_AFFILIATE_TAG',
-  'hourglass': 'https://www.amazon.com/dp/YOUR_PRODUCT_ID?tag=YOUR_AFFILIATE_TAG',
+  'time-capsule': 'https://amzn.to/48ngX1k',
+  'hourglass': 'https://amzn.to/3VWSjgt',
   
   // Add more products as needed
   // 'product-key': 'https://www.amazon.com/dp/PRODUCT_ID?tag=YOUR_AFFILIATE_TAG',
