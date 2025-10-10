aa67b0e7affd934c9afde3365c4887030582b932
fofariak <32439453+fofariak@users.noreply.github.com>
2025-10-10 21:06:04 +0530

    Update links.js

--- js/links.js
+++ js/links.js
@@ -29,7 +29,7 @@ const productLinks = {
   'nursing-pillow': 'https://amzn.to/4nMasde',
   'feeding-bottle': 'https://amzn.to/3L3rS6s',
   'opk-kit': 'https://amzn.to/48pYSzB',
-  'basal-thermometer': 'https://amzn.to/4obGM8W'
+  'basal-thermometer': 'https://amzn.to/4obGM8W',
   
   // Add more products as needed
   // 'product-key': 'https://www.amazon.com/dp/PRODUCT_ID?tag=YOUR_AFFILIATE_TAG',
