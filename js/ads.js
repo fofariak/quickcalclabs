a167b91248ac5abe23de3201ac410371f22c166a
Karan Fofaria <karanfofaria@Karans-MacBook-Air.local>
2025-11-22 17:27:42 +0530

    Fix: Use createElement instead of innerHTML for ad scripts to ensure proper execution

--- js/ads.js
+++ js/ads.js
@@ -2,18 +2,25 @@
 function renderAdBanner728x90(containerId) {
   const container = document.getElementById(containerId);
   if (container) {
-    container.innerHTML = `
-      <script type="text/javascript">
-        atOptions = {
-          'key' : '0f30d8d002656d29a062c88d9dd54fa9',
-          'format' : 'iframe',
-          'height' : 90,
-          'width' : 728,
-          'params' : {}
-        };
-      </script>
-      <script type="text/javascript" src="//www.highperformanceformat.com/0f30d8d002656d29a062c88d9dd54fa9/invoke.js"></script>
+    // Create config script
+    const configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : '0f30d8d002656d29a062c88d9dd54fa9',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
     `;
+    container.appendChild(configScript);
+    
+    // Create invoke script
+    const invokeScript = document.createElement('script');
+    invokeScript.type = 'text/javascript';
+    invokeScript.src = '//www.highperformanceformat.com/0f30d8d002656d29a062c88d9dd54fa9/invoke.js';
+    container.appendChild(invokeScript);
   }
 }
 
@@ -21,18 +28,25 @@ function renderAdBanner728x90(containerId) {
 function renderAdBanner320x50(containerId) {
   const container = document.getElementById(containerId);
   if (container) {
-    container.innerHTML = `
-      <script type="text/javascript">
-        atOptions = {
-          'key' : '1bd80cb650cefb9e21bdb1bb21def2c7',
-          'format' : 'iframe',
-          'height' : 50,
-          'width' : 320,
-          'params' : {}
-        };
-      </script>
-      <script type="text/javascript" src="//www.highperformanceformat.com/1bd80cb650cefb9e21bdb1bb21def2c7/invoke.js"></script>
+    // Create config script
+    const configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : '1bd80cb650cefb9e21bdb1bb21def2c7',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+      };
     `;
+    container.appendChild(configScript);
+    
+    // Create invoke script
+    const invokeScript = document.createElement('script');
+    invokeScript.type = 'text/javascript';
+    invokeScript.src = '//www.highperformanceformat.com/1bd80cb650cefb9e21bdb1bb21def2c7/invoke.js';
+    container.appendChild(invokeScript);
   }
 }
 
@@ -40,18 +54,25 @@ function renderAdBanner320x50(containerId) {
 function renderAdBanner300x250(containerId) {
   const container = document.getElementById(containerId);
   if (container) {
-    container.innerHTML = `
-      <script type="text/javascript">
-        atOptions = {
-          'key' : '21c3bdcbb595adb2c550f8c8d41ef140',
-          'format' : 'iframe',
-          'height' : 250,
-          'width' : 300,
-          'params' : {}
-        };
-      </script>
-      <script type="text/javascript" src="//www.highperformanceformat.com/21c3bdcbb595adb2c550f8c8d41ef140/invoke.js"></script>
+    // Create config script
+    const configScript = document.createElement('script');
+    configScript.type = 'text/javascript';
+    configScript.innerHTML = `
+      atOptions = {
+        'key' : '21c3bdcbb595adb2c550f8c8d41ef140',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+      };
     `;
+    container.appendChild(configScript);
+    
+    // Create invoke script
+    const invokeScript = document.createElement('script');
+    invokeScript.type = 'text/javascript';
+    invokeScript.src = '//www.highperformanceformat.com/21c3bdcbb595adb2c550f8c8d41ef140/invoke.js';
+    container.appendChild(invokeScript);
   }
 }
 
